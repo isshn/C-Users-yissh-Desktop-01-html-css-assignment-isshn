82f51ac9ebbea5341250f35e006fbaff38a86001
harada isshin <yisshin150@gmail.com>
2020-08-29 16:31:08 +0900

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-# 新規登録後のpath
-  def after_sign_up_path_for(resource)
-    posts_path(current_user)
-  end
-# ログイン後のpath
-  def after_sign_in_path_for(resource)
-    posts_path(current_user)
-  end
-
  #ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource)
     user_session_path
